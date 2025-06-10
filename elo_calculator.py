0273e31e23972e2f24f58ff4c6e4d843db54465e
a-salmeron <asalmeron@ucdavis.edu>
2025-06-10 11:49:30 -0700

    Delete elo_calculator.py

--- elo_calculator.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def calculate_historical_elos():
-    nba_all_elo = pd.read_csv('https://raw.githubusercontent.com/luke-lite/NBA-Prediction-Modeling/main/data/nbaallelo.csv')
-    
